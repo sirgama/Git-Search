a8a2cf2043d30fad01f46cf1576012ecbd083b5d
sirgama <sirengogamaliel@gmail.com>
2022-04-17 01:54:50 +0300

    st: add repositories  component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HeroComponent } from './Components/hero/hero.component';
 import { UsersComponent } from './Component/users/users.component';
+import { RepositoriesComponent } from './Component/repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeroComponent,
-    UsersComponent
+    UsersComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule

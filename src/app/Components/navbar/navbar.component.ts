8cc4d60a81e11565040d17519e70f1646e8b903a
sirgama <sirengogamaliel@gmail.com>
2022-04-19 01:34:04 +0300

    St: add image output binding on nabar image and name after a search

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from 'src/app/Services/user.service';
+import { Repositories } from 'src/app/Classes/repositories';
+import { Users } from 'src/app/Classes/users';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  searchItem = 'sirgama';
+  gitUser!: string;
+  repoSearch!:string;
+  user!: Users;
+  repo!: Repositories;
 
-  constructor() { }
+  findUser(username: any){
+    this.gitUser = '';
+    this.searchItem = username;
+    this.ngOnInit();
+  }
+
+
+  constructor(public githubUser: UserService, public gitRepos: UserService) { }
+
+
+
 
   ngOnInit(): void {
+    this.githubUser.getUser(this.searchItem);
+    this.user = this.githubUser.user;
+    this.gitRepos.getrepos(this.searchItem);
+    console.log(this.gitRepos)
   }
 
 }

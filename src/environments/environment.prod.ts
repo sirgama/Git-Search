b8bcc0bb06f4bb78a2b38cc4d529cbc889dcdc06
sirgama <sirengogamaliel@gmail.com>
2022-04-19 07:48:33 +0300

    ft: move credentials to environment production file and gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: '5a9d03c5494c6c1040d7',
+  gitToken: "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",
+  clientSecret: '91fc956d9fa4b721478230a16b044e904c48495c'
 };
